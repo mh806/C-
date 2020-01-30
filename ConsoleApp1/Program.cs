55b8fb01f5806c83347bf3d9925f463297fc9de3
william <william_mj_huang@hotmail.com>
2020-01-30 15:50:43 +0800

    C# Array Learning

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[,] matrix = new int[3,3];
+            for (int i = 0; i < matrix.GetLength(0); i++ )
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                matrix[i, j] = i * 3 + j;
+            Console.WriteLine(matrix);
+        }
+    }
+}
